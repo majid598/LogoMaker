b6d424ed98f966b9adafb72503f80c278c1478c5
Mr raju <ma8055965@gmail.com>
2024-06-23 22:58:26 -0700

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-export const server = "http://localhost:5000/"
+export const server = import.meta.env.VITE_SERVER
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
